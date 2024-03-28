3efba74f359bcc68121fb5e6073e31a7fa614010
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-03-28 17:02:43 -0500

    added ping

--- packages/p2p/src/Node.js
+++ packages/p2p/src/Node.js
@@ -2,9 +2,12 @@ import {createLibp2p} from 'libp2p'
 import {tcp} from '@libp2p/tcp'
 import {noise} from '@chainsafe/libp2p-noise'
 import {yamux} from '@chainsafe/libp2p-yamux'
+import {ping} from '@libp2p/ping'
+import {multiaddr} from 'multiaddr'
 
 export default class Node {
-  constructor(port) {
+  constructor(port = 0, ip = '127.0.0.1') {
+    this.ip = ip
     this.port = port
   }
 
@@ -12,11 +15,12 @@ export default class Node {
     this.node = await createLibp2p({
       addresses: {
         // add a listen address (localhost) to accept TCP connections on a random port
-        listen: [`/ip4/127.0.0.1/tcp/${this.port}`]
+        listen: [`/ip4/${this.ip}/tcp/${this.port}`],
       },
       transports: [tcp()],
       connectionEncryption: [noise()],
-      streamMuxers: [yamux()]
+      streamMuxers: [yamux()],
+      services: {ping: ping({protocolPrefix: 'ipfs'}),},
     })
     return this
   }
@@ -29,6 +33,16 @@ export default class Node {
     this.node.getMultiaddrs().forEach((addr) => {
       console.log(addr.toString())
     })
+    return this
+  }
+
+  get multiaddrs() {
+    return this.node.getMultiaddrs().map((addr) => addr.toString())
+  }
+
+  async ping(address) {
+    const latency = await this.node.services.ping.ping(multiaddr(address))
+    console.log(`${this.node.peerId.string} pinged ${address} in ${latency}ms`)
     return this
   }
 
--- packages/p2p/test/nodes.spec.js
+++ packages/p2p/test/nodes.spec.js
@@ -3,6 +3,10 @@ import Node from '../src/Node.js'
 
 describe('p2p', function (){
   let node1, node2
+  beforeEach(async function (){
+    if(node1) await node1.stop()
+    if(node2) await node2.stop()
+  })
   afterEach(async function (){
     await node1.stop()
     await node2.stop()
@@ -15,6 +19,14 @@ describe('p2p', function (){
     console.log('node1:', node1.node.peerId.string)
     console.log('node2:', node2.node.peerId.string)
     expect(node1.node.peerId.string).not.toEqual(node2.node.string)
+  })
+
+  it('should be able to ping a node', async function (){
+    node1 = await new Node(10001).create()
+    await node1.start()
+    node2 = await new Node(10002).create()
+    await node2.start()
+    await node1.ping(node2.multiaddrs[0])
   })
 
 })
