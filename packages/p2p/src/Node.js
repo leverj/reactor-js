3ec082a1e302d28aad9c6135809c93a59443b150
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-04-02 09:52:35 -0500

    updated

--- packages/p2p/src/Node.js
+++ packages/p2p/src/Node.js
@@ -8,26 +8,24 @@ import {gossipsub} from '@chainsafe/libp2p-gossipsub'
 
 
 export default class Node {
-  constructor(ip= '127.0.0.1', port = 0, isLeader = false) {
+  constructor(ip = '127.0.0.1', port = 0, isLeader = false) {
     this.ip = ip
     this.port = port
     this.isLeader = isLeader
     this.msgMap = {}
   }
 
+  get multiaddrs() { return this.node.getMultiaddrs().map((addr) => addr.toString()) }
+  get peerId() { return this.node.peerId.toString() }
+  get peers() { return this.node.getPeers().map((peer) => peer.toString()) }
+
   async create() {
     this.node = await createLibp2p({
-      addresses: {
-        // add a listen address (localhost) to accept TCP connections on a random port
-        listen: [`/ip4/${this.ip}/tcp/${this.port}`],
-      },
+      addresses: {listen: [`/ip4/${this.ip}/tcp/${this.port}`],},
       transports: [tcp()],
       connectionEncryption: [noise()],
       streamMuxers: [yamux()],
-      services: {
-        ping: ping({protocolPrefix: 'ipfs'}),
-        pubsub: gossipsub({emitSelf: true}),
-      },
+      services: {ping: ping({protocolPrefix: 'ipfs'}), pubsub: gossipsub({emitSelf: true}),},
     })
     return this
   }
@@ -37,18 +35,20 @@ export default class Node {
     return this
   }
 
-  get multiaddrs() {
-    return this.node.getMultiaddrs().map((addr) => addr.toString())
-  }
-
-  async ping(address) {
-    return await this.node.services.ping.ping(multiaddr(address))
-  }
+  async ping(address) { return await this.node.services.ping.ping(multiaddr(address)) }
 
   async connect(address) {
     await this.node.dial(multiaddr(address))
     return this
   }
+
+  async subscribe(peerId, topic, handler) {
+    this.node.services.pubsub.addEventListener('message', handler)
+    await this.node.services.pubsub.subscribe(topic)
+    await this.node.services.pubsub.connect(peerId)
+  }
+
+  async publish(topic, data) { await this.node.services.pubsub.publish(topic, new TextEncoder().encode(data)) }
 
   async stop() {
     await this.node.stop()
--- packages/p2p/test/nodes.gossipsub.spec.js
+++ packages/p2p/test/nodes.gossipsub.spec.js
@@ -1,5 +1,6 @@
-import { expect } from 'expect'
+import {expect} from 'expect'
 import Node from '../src/Node.js'
+import {setTimeout} from 'node:timers/promises'
 
 describe('p2p.gossipsub', function () {
   const ipv4 = '127.0.0.1'
@@ -12,38 +13,23 @@ describe('p2p.gossipsub', function () {
   })
 
   it.only('it should send data using gossipsub', async function () {
-    const delay = ms => new Promise(res => setTimeout(res, ms));
-    const depositReceipts = {}; // each node will just save the hash and ack. later children will sign and attest point to point
+    const depositReceipts = {} // each node will just save the hash and ack. later children will sign and attest point to point
     node1 = await new Node(ipv4, 9001, true).create().then(_ => _.start())
     const leaderAddr = await node1.multiaddrs[0]
     node2 = await new Node(ipv4, 9002).create().then(_ => _.start()).then(_ => _.connect(leaderAddr))
     node3 = await new Node(ipv4, 9003).create().then(_ => _.start()).then(_ => _.connect(leaderAddr))
     node4 = await new Node(ipv4, 9004).create().then(_ => _.start()).then(_ => _.connect(leaderAddr))
-    const handler = ({ connection, stream, protocol }) => {
-       console.log('#####################################################handle')
+    for (const node of [node2, node3, node4]) {
+      await node.subscribe(node1.peerId, 'DepositHash', (message) => depositReceipts[node.peerId] = new TextDecoder().decode(message.detail.data))
     }
-    for (const node of [node2, node3, node4]){
-      console.log('Child node Peer of Node1', node.node.peerId.toString())
-      node.node.handle('/meshsub/1.1.0', handler, {
-        maxInboundStreams: 5,
-        maxOutboundStreams: 5
-      })
-      node.node.services.pubsub.addEventListener('message', (message) => {
-        console.log(`Recd Deposit Hash from Leader, by Child Node ${node.node.peerId.toString()} on topic ${message.detail.topic}:`, new TextDecoder().decode(message.detail.data))
-        depositReceipts[node.node.peerId.toString()] = new TextDecoder().decode(message.detail.data)
-      })
-      await node1.node.services.pubsub.connect(node.node.peerId.toString())
-      await node.node.services.pubsub.subscribe('DepositHash')
-      //await node.node.services.pubsub.start()
-    }
-    
-    await delay(1000)
-    const depositHash = '0xbef807c488b8a3db6834ee242ff888e9ebb5961deb9323c8da97853b43755aab'
-    await node1.node.services.pubsub.publish('DepositHash', new TextEncoder().encode(depositHash))
-    await delay(1000)
-    for (const peerOfNode1 of  node1.node.getPeers()){
+    await setTimeout(100)
+    let depositHash = '0xbef807c488b8a3db6834ee242ff888e9ebb5961deb9323c8da97853b43755aab'
+    await node1.publish('DepositHash', depositHash)
+    await setTimeout(100)
+    expect(node1.peers.length).toEqual(3)
+    for (const peerOfNode1 of node1.peers) {
       expect(depositReceipts[peerOfNode1.toString()]).toEqual(depositHash)
     }
-  })
+  }).timeout(10000)
 
 })
