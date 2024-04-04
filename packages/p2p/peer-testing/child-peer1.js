26472d4b89e424b8f69a93449c7fbc87b4e2887a
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-04-04 09:50:32 -0500

    updated

--- packages/p2p/peer-testing/child-peer1.js
+++ packages/p2p/peer-testing/child-peer1.js
@@ -1,6 +1,6 @@
 import Node from '../src/Node.js'
 
-const peerJson = {
+const peerIdJson = {
   privKey: 'CAESQGOEED1xY75lT0dqKQ1py7iYryEd1OB+l+6Co1XvUYgVV/OuL7KfE2VGxFOxmbkOyjcVdGp3otRdTnKXWvF4OBc',
   pubKey: 'CAESIFfzri+ynxNlRsRTsZm5Dso3FXRqd6LUXU5yl1rxeDgX',
   id: '12D3KooWFjh9hF2Hnj5ctFDxhz2N2zFin3Wc3P9umGWogMycKme6'
@@ -8,7 +8,7 @@ const peerJson = {
 const leaderPeerId = '12D3KooWRRqAo5f41sQmc9BpsfqarZgd7PWUiX14Mz1htXDEc7Gp'
 const leaderMuladdr = `/ip4/51.159.143.255/tcp/8080/p2p/${leaderPeerId}`
 
-const node = await new Node({ip: '0.0.0.0', port: 8081, isLeader: true, peerJson}).create()
+const node = await new Node({ip: '0.0.0.0', port: 8081, isLeader: true, peerIdJson}).create()
 await node.start()
 await node.connect(leaderMuladdr)
 
--- packages/p2p/peer-testing/leader-peer.js
+++ packages/p2p/peer-testing/leader-peer.js
@@ -1,12 +1,12 @@
 import Node from '../src/Node.js'
-const peerJson  = {
+const peerIdJson  = {
   privKey: 'CAESQK0/fGhAG26fRXLTxDyV7LpSreIfOXSJ+krI+BdTbeJq5/UphgwH8/mDsTa9HebrBuDJ6EtxNwnEAjEVyA/OQjU',
   pubKey: 'CAESIOf1KYYMB/P5g7E2vR3m6wbgyehLcTcJxAIxFcgPzkI1',
   id: '12D3KooWRRqAo5f41sQmc9BpsfqarZgd7PWUiX14Mz1htXDEc7Gp'
 }
 
-// const node = await new Node({ip: '51.159.143.255', port: 8080, isLeader: true, peerJson}).create()
-const node = await new Node({ip: '0.0.0.0', port: 8080, isLeader: true, peerJson}).create()
+// const node = await new Node({ip: '51.159.143.255', port: 8080, isLeader: true, peerIdJson}).create()
+const node = await new Node({ip: '0.0.0.0', port: 8080, isLeader: true, peerIdJson}).create()
 await node.start()
 setInterval( _=> node.publish('DepositHash', `love from leader ${Date.now()}`).catch(e=>{
   if(e.message !== 'PublishError.NoPeersSubscribedToTopic') console.log(e)
