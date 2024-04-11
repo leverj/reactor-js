b78c7fd0e1d6cab6f6031445d8e97f90d6590673
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-04-10 22:56:37 -0500

    dkg

--- packages/p2p/test/dkg.spec.js
+++ packages/p2p/test/dkg.spec.js
@@ -8,6 +8,16 @@ describe('dkg', function () {
   before(async function () {
     await bls.init()
   })
+
+  function verify(message, members, start, total) {
+    const {signs, signers} = signMessage(message, members)
+    const groupsSign = new bls.Signature()
+    groupsSign.recover(signs.splice(start, total), signers.splice(start, total))
+    const verified = members[0].groupPublicKey.verify(groupsSign, message)
+    groupsSign.clear()
+    return verified
+  }
+
   it('should be able to create distributed keys and sign message', async function () {
     const threshold = 4
     const members = createDkgMembers([10314, 30911, 25411, 8608, 31524, 15441, 23399], threshold)
@@ -16,12 +26,11 @@ describe('dkg', function () {
       expect(member.groupPublicKey).toEqual(members[0].groupPublicKey)
     }
     const message = 'hello world'
-    const {signs, signers} = signMessage(message, members)
-    const groupsSig = new bls.Signature()
-    groupsSig.recover(signs.splice(1, 4), signers.splice(1, 4))
-    var verified = members[0].groupPublicKey.verify(groupsSig, message)
-    expect(verified).toBe(true)
-    groupsSig.clear()
+    expect(verify(message, members, 0, 3)).toBe(false)
+    expect(verify(message, members, 0, 4)).toBe(true)
+    expect(verify(message, members, 0, 5)).toBe(true)
+    expect(verify(message, members, 0, 6)).toBe(true)
+    expect(verify(message, members, 0, 7)).toBe(true)
   })
 
   it('should be able to get shared public key from verification vector', async function () {
@@ -70,6 +79,52 @@ describe('dkg', function () {
     const {signs: newSigns, signers: newSigners} = signMessage(message, members)
     const newGroupsSign = new bls.Signature()
     newGroupsSign.recover(newSigns.splice(4, 4), newSigners.splice(4, 4))
+    //fixme: test fails here
     expect(members[0].groupPublicKey.verify(newGroupsSign, message)).toBe(true)
+  })
+
+  it('should be able to remove a member', async function () {
+    const threshold = 4
+    const members = createDkgMembers([10314, 30911, 25411, 8608, 31524, 23399, 15441, 138473], threshold)
+    const message = 'hello world'
+    const groupsPublicKey = members[0].groupPublicKey
+    members.forEach(member => member.reinitiate())
+    members.pop()
+    setupMembers(members, threshold)
+    for (const member of members) expect(member.groupPublicKey.serializeToHexStr()).toEqual(groupsPublicKey.serializeToHexStr())
+    expect(verify(message, members, 0, 3)).toBe(false)
+    expect(verify(message, members, 0, 4)).toBe(true)
+    expect(verify(message, members, 0, 5)).toBe(true)
+    expect(verify(message, members, 3, 6)).toBe(true)
+    expect(verify(message, members, 4, 6)).toBe(false)
+  })
+
+  it('should be able to increase threshold', async function () {
+    const threshold = 4
+    const message = 'hello world'
+    const members = createDkgMembers([10314, 30911, 25411, 8608, 31524, 23399, 15441, 138473], threshold)
+    expect(verify(message, members, 0, 4)).toBe(true)
+    const groupsPublicKey = members[0].groupPublicKey
+    members.forEach(member => member.reinitiate())
+    setupMembers(members, threshold + 1)
+    for (const member of members) expect(member.groupPublicKey.serializeToHexStr()).toEqual(groupsPublicKey.serializeToHexStr())
+    expect(verify(message, members, 0, 4)).toBe(false)
+    expect(verify(message, members, 0, 5)).toBe(true)
+  })
+
+  it('should be able to decrease threshold', async function () {
+    const threshold = 5
+    const message = 'hello world'
+    const members = createDkgMembers([10314, 30911, 25411, 8608, 31524, 23399, 15441, 138473], threshold)
+    expect(verify(message, members, 0, 4)).toBe(false)
+    expect(verify(message, members, 0, 5)).toBe(true)
+    const groupsPublicKey = members[0].groupPublicKey
+    members.forEach(member => member.reinitiate())
+    setupMembers(members, threshold - 1)
+    for (const member of members) expect(member.groupPublicKey.serializeToHexStr()).toEqual(groupsPublicKey.serializeToHexStr())
+    expect(verify(message, members, 0, 3)).toBe(false)
+    //fixme: test fails here
+    expect(verify(message, members, 0, 4)).toBe(true)
+    expect(verify(message, members, 0, 5)).toBe(true)
   })
 })
