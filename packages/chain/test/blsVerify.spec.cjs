1e45c73673054c1e2399f52f652b1576a496f8f6
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-04-17 09:51:19 -0500

    contract verification

--- packages/chain/test/blsVerify.spec.cjs
+++ packages/chain/test/blsVerify.spec.cjs
@@ -1,5 +1,7 @@
 const {ethers:{deployContract, getContractFactory, getSigners}}  = require('hardhat')
 const {expect} = require('expect')
+const {fromHexString, toHexString} = require('@chainsafe/ssz')
+const BigNumber = require('bignumber.js')
 
 describe('dlsVerify', () => {
   let contract, owner, anyone
@@ -11,10 +13,14 @@ describe('dlsVerify', () => {
 
   describe('verify', () => {
     it('should verify name', async () => {
-      let someName = 'some name'
-      contract = await deployContract('BlsVerify', [someName])
-      const verified = await contract.verify()
-      expect(verified).toEqual(someName)
+      contract = await deployContract('BlsVerify', [])
+      const groupsSign =  '0x50d128055e103463979742916a1073899ce06108e1fe802f202c8daeb1f63408'
+      const message =  'hello world'
+      const publicKey  = '0xfc9f1b02cab189120e0e8d77c75b6e4e833f942060806736d50f752f4e4348147e0104c15e9404c4c33bc5694bc1367ad893266d8fac944be98eb5332aae051b'
+      console.log('groupsSign', BigNumber(groupsSign).toFixed())
+      console.log('publicKey', BigNumber(publicKey).toFixed())
+      // const verified = await contract.verify()
+      // expect(verified).toEqual(someName)
     })
   })
 })
