1869215fa19ea51e24bb80f646b0b26a3e8503f4
sachetsingh77 <sachet@leverj.io>
2024-04-19 10:44:11 +0530

    compilable version of BLSTest

--- packages/chain/contracts/BlsVerify.sol
+++ packages/chain/contracts/BlsVerify.sol
@@ -26,7 +26,7 @@ contract BlsVerify {
     constructor () {
     }
 
-    function verify(uint256[2] memory signature, uint256[4] memory pubkey, uint256[2] memory message) internal view returns (bool) {
+    function verify(uint256[2] memory signature, uint256[4] memory pubkey, uint256[2] memory message) public view returns (bool) {
         uint256[12] memory input = [signature[0], signature[1], nG2x1, nG2x0, nG2y1, nG2y0, message[0], message[1], pubkey[1], pubkey[0], pubkey[3], pubkey[2]];
         uint256[1] memory out;
         bool success;
--- packages/chain/test/blsVerify.spec.cjs
+++ packages/chain/test/blsVerify.spec.cjs
@@ -19,8 +19,18 @@ describe('dlsVerify', () => {
       const publicKey  = '0xfc9f1b02cab189120e0e8d77c75b6e4e833f942060806736d50f752f4e4348147e0104c15e9404c4c33bc5694bc1367ad893266d8fac944be98eb5332aae051b'
       console.log('groupsSign', BigNumber(groupsSign).toFixed())
       console.log('publicKey', BigNumber(publicKey).toFixed())
-      // const verified = await contract.verify()
-      // expect(verified).toEqual(someName)
+      const verified = await contract.verify([1,2], [1,2,3,4], [1,2])
+      console.log('verified', verified)
+      expect(verified).toEqual(true)
+    })
+    it('should run BGLS', async () => {
+      contract = await deployContract('BLSExample', [])
+      console.log(contract)
+      await contract.verifyBLSTest()
+      const verified = await contract.result()
+      console.log('BLSTest', verified)
+      expect(verified).toEqual(true)
+      
     })
   })
 })
