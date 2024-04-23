338f882083e128fcda1c953ff47aabeb7b84d444
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-04-23 12:57:43 -0500

    updated

--- packages/p2p/test/mcl-bls.spec.js
+++ packages/p2p/test/mcl-bls.spec.js
@@ -19,45 +19,83 @@ describe('mcl-bls', () => {
 
   it('bls should verify mcl signature', async function () {
     const secretHex = 'a3e9769b84c095eca6b98449ac86b6e2c589834fe24cb8fbb7b36f814fd06113'
-    const map = new Keymap()
-    mcl.setMappingMode(mcl.MAPPING_MODE_TI)
-    mcl.setDomain('testing evmbls')
-    const message = stringToHex(messageString)
-    map.mcl.secret = secretFromHex(secretHex)
-    map.mcl.pubkey = mcl.getPublicKey(secretHex)
-    const {signature, M} = mcl.sign(message, map.mcl.secret)
-    map.mcl.signature = signature
+    const map = new Keymap(messageString).replenish(secretHex).print()
+    expect(map.bls.pubkey.serializeToHexStr()).toEqual(map.mcl.pubkey.serializeToHexStr())
+    expect(map.bls.signature.serializeToHexStr()).toEqual(map.mcl.signature.serializeToHexStr())
+  })
 
-    map.serialized.pubkey = bls.deserializeHexStrToPublicKey(map.mcl.pubkey.serializeToHexStr())
-    map.serialized.secret = bls.deserializeHexStrToSecretKey(map.mcl.secret.serializeToHexStr())
-    map.serialized.signature = bls.deserializeHexStrToSignature(map.mcl.signature.serializeToHexStr())
-
-    map.bls.secret = map.serialized.secret
-    map.bls.pubkey = map.bls.secret.getPublicKey()
-    map.bls.signature = map.bls.secret.sign(messageString)
-    map.print()
+  it('different secrets for different domains', async function () {
+    const secretHex = 'a3e9769b84c095eca6b98449ac86b6e2c589834fe24cb8fbb7b36f814fd06113'
+    new Keymap(messageString, 'testing evmbls').replenish(secretHex).printSignatures()
+    new Keymap(messageString, 'testing evmbls2').replenish(secretHex).printSignatures()
+    new Keymap(messageString, 'something').replenish(secretHex).printSignatures()
   })
 })
 
 class Keymap {
-  constructor() {
+  constructor(messageString, domain = 'testing evmbls') {
+    this.messageString = messageString
+    mcl.setMappingMode(mcl.MAPPING_MODE_TI)
+    mcl.setDomain(domain)
     this.mcl = {secret: null, pubkey: null, signature: null}
     this.bls = {secret: null, pubkey: null, signature: null}
     this.serialized = {secret: null, pubkey: null, signature: null}
   }
+  replenish(secretHex) {
+    this.replenishMcl(secretHex)
+    this.serializeBls()
+    this.setBls()
+    return this
+  }
+  replenishMcl(secretHex) {
+    this.mcl.secret = secretFromHex(secretHex)
+    this.mcl.pubkey = mcl.getPublicKey(secretHex)
+    this.mcl.signature = mcl.sign(stringToHex(this.messageString), this.mcl.secret).signature
+    return this
+  }
+
+  serializeBls() {
+    this.serialized.secret = bls.deserializeHexStrToSecretKey(this.mcl.secret.serializeToHexStr())
+    this.serialized.pubkey = bls.deserializeHexStrToPublicKey(this.mcl.pubkey.serializeToHexStr())
+    this.serialized.signature = bls.deserializeHexStrToSignature(this.mcl.signature.serializeToHexStr())
+    return this
+  }
+
+  setBls() {
+    this.bls.secret = this.serialized.secret
+    this.bls.pubkey = this.bls.secret.getPublicKey()
+    this.bls.signature = this.bls.secret.sign(this.messageString)
+    return this
+  }
 
   print() {
-    console.log('secret')
-    console.log('mcl', this.mcl.secret.serializeToHexStr())
-    console.log('ser', this.serialized.secret.serializeToHexStr())
-    console.log('bls', this.bls.secret.serializeToHexStr())
-    console.log('public key')
-    console.log('mcl', this.mcl.pubkey.serializeToHexStr())
-    console.log('ser', this.serialized.pubkey.serializeToHexStr())
-    console.log('bls', this.bls.pubkey.serializeToHexStr())
+    this.printSecrets()
+    this.printPublicKeys()
+    this.printSignatures()
+    return this
+  }
+
+  printSignatures() {
     console.log('signature')
     console.log('mcl', this.mcl.signature.serializeToHexStr())
     console.log('ser', this.serialized.signature.serializeToHexStr())
     console.log('bls', this.bls.signature.serializeToHexStr())
+    return this
+  }
+
+  printPublicKeys() {
+    console.log('public key')
+    console.log('mcl', this.mcl.pubkey.serializeToHexStr())
+    console.log('ser', this.serialized.pubkey.serializeToHexStr())
+    console.log('bls', this.bls.pubkey.serializeToHexStr())
+    return this
+  }
+
+  printSecrets() {
+    console.log('secret')
+    console.log('mcl', this.mcl.secret.serializeToHexStr())
+    console.log('ser', this.serialized.secret.serializeToHexStr())
+    console.log('bls', this.bls.secret.serializeToHexStr())
+    return this
   }
 }
