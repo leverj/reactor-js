35de407450da8349b84a803f79a6e14096ef7433
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-05-01 10:12:50 -0500

    Node to NetworkNode

--- packages/p2p/test/nodes.spec.js
+++ packages/p2p/test/nodes.spec.js
@@ -1,13 +1,13 @@
 import {expect} from 'expect'
 import {setTimeout} from 'node:timers/promises'
-import {peerIdJsons, startNodes, stopNodes} from './help/index.js'
+import {peerIdJsons, startNetworkNodes, stopNetworkNodes} from './help/index.js'
 
 describe('p2p', function () {
   const meshProtocol = '/mesh/1.0.0'
-  afterEach(stopNodes)
+  afterEach(stopNetworkNodes)
 
   it('it should start node with known peerId and should be able to connect', async function () {
-    const [node1, node2] = await startNodes(2)
+    const [node1, node2] = await startNetworkNodes(2)
     expect(node1.peerId).toEqual(peerIdJsons[0].id)
     expect(node2.peerId).toEqual(peerIdJsons[1].id)
     expect(node1.peers.length).toEqual(0)
@@ -16,13 +16,13 @@ describe('p2p', function () {
   })
 
   it('should be able to ping a node', async function () {
-    const [node1, node2] = await startNodes(2)
+    const [node1, node2] = await startNetworkNodes(2)
     const latency = await node1.ping(node2.multiaddrs[0])
     expect(latency).toBeGreaterThan(0)
   })
 
   it('spokes should be able to dial the hub, and become peers of the hub', async function () {
-    const [hub, spoke1, spoke2, spoke3] = await startNodes(4, true)
+    const [hub, spoke1, spoke2, spoke3] = await startNetworkNodes(4, true)
     expect(hub.peers.length).toEqual(3)
     expect(spoke1.peers.length).toEqual(1)
     expect(spoke2.peers.length).toEqual(1)
@@ -35,7 +35,7 @@ describe('p2p', function () {
   it('it should send data across stream from node1 to node2', async function () {
     const messageRecd = {}
     const responses = {}
-    const [node1, node2, node3, node4] = await startNodes(4, true)
+    const [node1, node2, node3, node4] = await startNetworkNodes(4, true)
     node1.registerStreamHandler(meshProtocol, async (stream, peerId, msg) => {
       messageRecd[peerId] = msg
       node1.sendMessage(stream, `responding ${msg}`)
@@ -66,7 +66,7 @@ describe('p2p', function () {
 
   it('it should send data using gossipsub', async function () {
     const depositReceipts = {} // each node will just save the hash and ack. later children will sign and attest point to point
-    const [leader, node2, node3, node4] = await startNodes(4, true)
+    const [leader, node2, node3, node4] = await startNetworkNodes(4, true)
     for (const node of [node2, node3, node4]) {
       await node.connectPubSub(
         leader.peerId,
@@ -93,7 +93,7 @@ describe('p2p', function () {
 
   // fixme: to be implemented
   it.skip('should not allow to connect a node if not approved', async function () {
-    const [node1, node2] = await startNodes(2)
+    const [node1, node2] = await startNetworkNodes(2)
     node1.connect(node2.multiaddrs[0])
     await setTimeout(100)
     expect(node2.peers.length).toEqual(0)
