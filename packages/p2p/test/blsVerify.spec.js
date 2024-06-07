2c744fc3365c3967c658174aeca7375c9fc897c9
sachetsingh77 <sachet@leverj.io>
2024-06-07 15:23:56 +0530

    sign blockchain event message

--- packages/p2p/test/blsVerify.spec.js
+++ packages/p2p/test/blsVerify.spec.js
@@ -1,10 +1,14 @@
-import {expect} from 'expect'
-import {deployContract, getSigners, createDkgMembers, signMessage} from './help/index.js'
+import {setTimeout} from 'timers/promises'
+import { expect } from 'expect'
+import axios from 'axios'
+import { deployContract, getSigners, createDkgMembers, signMessage } from './help/index.js'
 import bls from '../src/bls.js'
 import * as mcl from '../src/mcl/mcl.js'
-import {deserializeHexStrToG1, deserializeHexStrToG2} from 'mcl-wasm'
-import {stringToHex} from '../src/mcl/mcl.js'
-import {createApiNodes, createFrom, createInfo_json, deleteInfoDir, getInfo, getPublicKey, getWhitelists, killChildProcesses, publishWhitelist, startDkg, stop, waitForWhitelistSync} from './help/e2e.js'
+import { deserializeHexStrToG1, deserializeHexStrToG2 } from 'mcl-wasm'
+import { stringToHex } from '../src/mcl/mcl.js'
+import {tryAgainIfConnectionError, waitToSync} from '../src/utils.js'
+
+import { createApiNodes, createFrom, createInfo_json, deleteInfoDir, getInfo, getPublicKey, getWhitelists, killChildProcesses, publishWhitelist, startDkg, stop, waitForWhitelistSync } from './help/e2e.js'
 
 
 const messageString = 'hello world'
@@ -19,32 +23,37 @@ describe('blsVerify', () => {
     L1DepositContract = await deployContract('L1Deposit', [])
   })
 
-  it.skip('deposit on L1 and listen on emitted event', async function(){
+  it.skip('deposit on L1 and listen on emitted event', async function () {
     const tx = await L1DepositContract.deposit(20)
-    console.log(tx)
     const receipt = await tx.wait()
+    
     const allNodes = [9000, 9001, 9002, 9003]
     await createInfo_json(allNodes.length)
 
     await createApiNodes(allNodes.length)
-    // await setTimeout(2000)
-    const txnHash = 'hash123456' //Get this from above event receipt
-    await axios.post('http://localhost:9000/api/tss/aggregateSign', {txnHash, 'msg': message})
-    const fn = async () => {
-      const {data: {verified}} = await axios.get('http://localhost:9000/api/tss/aggregateSign?txnHash=' + txnHash)
-      return verified
+    await setTimeout(1000)
+    for (const event of receipt.events) {
+      if (event.event !== 'L1DepositByUser') continue;
+      const message = JSON.stringify(event.args)
+      const txnHash = event.transactionHash
+      await axios.post('http://localhost:9000/api/tss/aggregateSign', { txnHash, 'msg': message })
+      const fn = async () => {
+        const { data: { verified } } = await axios.get('http://localhost:9000/api/tss/aggregateSign?txnHash=' + txnHash)
+        return verified
+      }
+      await waitToSync([fn], 200)
+      const { data: { verified } } = await axios.get('http://localhost:9000/api/tss/aggregateSign?txnHash=' + txnHash)
+      expect(verified).toEqual(true)
+
     }
-    await waitToSync([fn], 200)
-    const {data: {verified}} = await axios.get('http://localhost:9000/api/tss/aggregateSign?txnHash=' + txnHash)
-    expect(verified).toEqual(true)
-    
+
   })
   it('verify single signature', async function () {
     // mcl.setMappingMode(mcl.MAPPING_MODE_TI)
     // mcl.setDomain('testing evmbls')
     const message = mcl.stringToHex(messageString)
-    const {pubkey, secret} = mcl.newKeyPair()
-    const {signature, M} = mcl.sign(message, secret)
+    const { pubkey, secret } = mcl.newKeyPair()
+    const { signature, M } = mcl.sign(message, secret)
     let sig_ser = mcl.g1ToBN(signature)
     let pubkey_ser = mcl.g2ToBN(pubkey)
     let message_ser = mcl.g1ToBN(M)
@@ -56,7 +65,7 @@ describe('blsVerify', () => {
   it('should verify signature from dkgnodes', async function () {
     const threshold = 4
     const members = await createDkgMembers([10314, 30911, 25411, 8608, 31524, 15441, 23399], threshold)
-    const {signs, signers} = signMessage(messageString, members)
+    const { signs, signers } = signMessage(messageString, members)
     const groupsSign = new bls.Signature()
     groupsSign.recover(signs, signers)
 
