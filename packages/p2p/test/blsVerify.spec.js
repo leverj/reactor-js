90df3c3cfa4c20eb2884603cd03d3211cc19152b
sachetsingh77 <sachet@leverj.io>
2024-06-07 10:57:20 +0530

    checkin TC changes with skip()

--- packages/p2p/test/blsVerify.spec.js
+++ packages/p2p/test/blsVerify.spec.js
@@ -4,6 +4,8 @@ import bls from '../src/bls.js'
 import * as mcl from '../src/mcl/mcl.js'
 import {deserializeHexStrToG1, deserializeHexStrToG2} from 'mcl-wasm'
 import {stringToHex} from '../src/mcl/mcl.js'
+import {createApiNodes, createFrom, createInfo_json, deleteInfoDir, getInfo, getPublicKey, getWhitelists, killChildProcesses, publishWhitelist, startDkg, stop, waitForWhitelistSync} from './help/e2e.js'
+
 
 const messageString = 'hello world'
 describe('blsVerify', () => {
@@ -17,9 +19,25 @@ describe('blsVerify', () => {
     L1DepositContract = await deployContract('L1Deposit', [])
   })
 
-  it.only('deposit on L1 and listen on emitted event', async function(){
-    let result = await L1DepositContract.deposit(1)
-    console.log(result)
+  it.skip('deposit on L1 and listen on emitted event', async function(){
+    const tx = await L1DepositContract.deposit(20)
+    console.log(tx)
+    const receipt = await tx.wait()
+    const allNodes = [9000, 9001, 9002, 9003]
+    await createInfo_json(allNodes.length)
+
+    await createApiNodes(allNodes.length)
+    // await setTimeout(2000)
+    const txnHash = 'hash123456' //Get this from above event receipt
+    await axios.post('http://localhost:9000/api/tss/aggregateSign', {txnHash, 'msg': message})
+    const fn = async () => {
+      const {data: {verified}} = await axios.get('http://localhost:9000/api/tss/aggregateSign?txnHash=' + txnHash)
+      return verified
+    }
+    await waitToSync([fn], 200)
+    const {data: {verified}} = await axios.get('http://localhost:9000/api/tss/aggregateSign?txnHash=' + txnHash)
+    expect(verified).toEqual(true)
+    
   })
   it('verify single signature', async function () {
     // mcl.setMappingMode(mcl.MAPPING_MODE_TI)
