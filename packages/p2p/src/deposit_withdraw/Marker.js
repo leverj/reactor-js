9a8d679aab4b0d26c5807da1348706a4e5b8b5fa
sachetsingh77 <sachet@leverj.io>
2024-06-13 12:15:33 +0530

    traker initial checkins

--- packages/p2p/src/deposit_withdraw/Marker.js
+++ packages/p2p/src/deposit_withdraw/Marker.js
@@ -1,3 +1,4 @@
+import {mkdir, readFile, rm, writeFile} from 'node:fs/promises'
 
 export class Marker{
     constructor(chainId, block){
@@ -6,7 +7,8 @@ export class Marker{
     }
     getBlock(){return this.block}
     setBlock(block){this.block = block}
-    serialize(){}
+    filePath(){return ""}
+    async serialize(){await writeFile(filePath(), JSON.stringify({chainId: this.chainId, block: this.block}))}
     deserialize(){}
     /*static getMarker(chainId){return new Marker(chainId, 0)} //read from file and return Marker object 
     get block () {return this.block}
