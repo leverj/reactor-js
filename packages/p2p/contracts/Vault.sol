a318354b70e2fcd68415dcb4a1df571e9ab8d867
sachetsingh77 <sachet@leverj.io>
2024-06-18 13:30:44 +0530

    validate pub key, hashToPoint and messageString is the hashOf

--- packages/p2p/contracts/Vault.sol
+++ packages/p2p/contracts/Vault.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 import "hardhat/console.sol";
+import './BlsVerify.sol';
+
 contract Vault {
 
     address constant public ETH = address(0);
+    string constant cipher_suite_domain = 'BN256-HASHTOPOINT';
 
     event Deposited(address indexed depositor, address indexed tokenAddress, uint toChainId, uint amount, uint depositCounter);
     event Withdrawn(address depositor, uint amount);
@@ -15,24 +18,30 @@ contract Vault {
     mapping(bytes32 => bool) public deposited;
     uint public depositCounter = 0;
 
-    constructor() {
-    }
+    BlsVerify verifier;
 
+    constructor() {
+        verifier = new BlsVerify();
+    }
+    //FIXME who can call this ? isOwner or some multi-sig operation like a DAO op?
+    function setPublicKey(uint[4] memory publicKey) external {
+        pubkey = publicKey;
+    }
     function depositEth(uint toChainId) external payable {
         pool[ETH] += msg.value;
         uint counter = depositCounter++;
-        bytes32 hash = keccak256(abi.encodePacked(msg.sender, ETH, toChainId, msg.value, counter));
+        bytes32 hash = hashOf(msg.sender, ETH, toChainId, msg.value, counter);
         deposited[hash] = true;
+        console.logString('depositEth');
         console.logBytes32(hash);
         emit Deposited(msg.sender, ETH, toChainId, msg.value, counter);
     }
     //This function will be called both externally and internally, hence public. 
     //Clients could calculate the hash off-chain also and then simply query the public deposited map above.
     function isDeposited(address depositor, address token, uint toChainId, uint amount, uint counter) public view returns (bool){
-        bytes32 hash = keccak256(abi.encodePacked(depositor, token, toChainId, amount, counter));
+        bytes32 hash = hashOf(depositor, token, toChainId, amount, counter);
         return deposited[hash];
     }
-    //Utility function for unit testing
     function hashOf(address depositor, address token, uint toChainId, uint amount, uint counter) public view returns (bytes32){
         return keccak256(abi.encodePacked(depositor, token, toChainId, amount, counter));
     }
@@ -51,5 +60,19 @@ contract Vault {
 
     function disburse(uint amount) external {
         emit Withdrawn(address(0), amount);
+    }
+
+    function verifySignature(uint256[2] memory signature, uint256[4] memory signerKey, uint256[2] memory message, string memory messageString, address depositor, address token, uint toChainId, uint amount, uint counter) public view returns (bool) {
+        require(pubkey.length == signerKey.length, 'Invalid Public Key length');
+        require((pubkey[0] == signerKey[0] && pubkey[1] == signerKey[1] && pubkey[2] == signerKey[2] && pubkey[3] == signerKey[3]), 'Invalid Public Key');
+        bytes32 hashOf = hashOf(depositor, token, toChainId, amount, counter);
+        uint256[2] memory hashToPoint = verifier.hashToPoint(bytes(cipher_suite_domain), bytes(messageString));
+        require(hashToPoint[0] == message[0] && hashToPoint[1] == message[1], 'Invalid hash to point');
+        console.logBytes32(hashOf);
+        console.logString(messageString);
+        bool validSignature = verifier.verifySignature(signature, signerKey, message);
+        require(validSignature == true, 'Invalid Signature');
+        //process business data now like minting burning
+        return validSignature;
     }
 }
--- packages/p2p/test/vault.spec.js
+++ packages/p2p/test/vault.spec.js
@@ -1,17 +1,31 @@
-import {expect} from 'expect'
-import {createVault, provider} from './help/vault.js'
-import {Interface} from 'ethers'
+import { expect } from 'expect'
+import { createVault, provider } from './help/vault.js'
+import { Interface } from 'ethers'
 import vaultAbi from '../src/abi/Vault.json' assert {type: 'json'}
-import {buffersToHex, keccak256, toBuffer, uint} from '@leverj/gluon-plasma.common/src/utils/ethereum.js'
+import { buffersToHex, keccak256, toBuffer, uint } from '@leverj/gluon-plasma.common/src/utils/ethereum.js'
+
+import { deployContract, getSigners, createDkgMembers, signMessage } from './help/index.js'
+import bls from '../src/utils/bls.js'
+
+const cipher_suite_domain = 'BN256-HASHTOPOINT';
+bls.setDomain(cipher_suite_domain);
 describe('vault contract', function () {
 
   it.only('should be able to deposit ether', async function () {
+    const threshold = 4
+    const members = await createDkgMembers([10314, 30911, 25411, 8608, 31524, 15441, 23399], threshold)
+    const pubkeyHex = members[0].groupPublicKey.serializeToHexStr()
+    const pubkey = bls.deserializeHexStrToG2(pubkeyHex)
+    let pubkey_ser = bls.g2ToBN(pubkey)
+    let member_pub_key = bls.deserializeHexStrToG2(members[0].publicKey.serializeToHexStr())
+    let member_pub_key_g2 = bls.g2ToBN(member_pub_key)
     const contract = await createVault()
+    await contract.setPublicKey(pubkey_ser)
     const amount = BigInt(1e+6)
-    const toChain  = 10101
-    const txnReceipt = await contract.depositEth(toChain, {value: amount})
+    const toChain = 10101
+    const txnReceipt = await contract.depositEth(toChain, { value: amount })
     const logs = await provider.getLogs(txnReceipt)
-    for (const log of logs){
+    for (const log of logs) {
       const parsedLog = new Interface(vaultAbi.abi).parseLog(log)
       let isDeposited = await contract.isDeposited(parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]), BigInt(parsedLog.args[3]), BigInt(parsedLog.args[4]))
       expect(isDeposited).toEqual(true)
@@ -20,10 +34,32 @@ describe('vault contract', function () {
       expect(isDeposited).toEqual(false)
       isDeposited = await contract.isDeposited(parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]), BigInt(parsedLog.args[4]), BigInt(parsedLog.args[3]))
       expect(isDeposited).toEqual(false)
+      const hashOf = await contract.hashOf(parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]), BigInt(parsedLog.args[3]), BigInt(parsedLog.args[4]));
+      console.log("hashOf", hashOf);
       //Compute hash off-chain and check deposit status
       const hashOffChain = keccak256(parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]).toString(), BigInt(parsedLog.args[3]).toString(), BigInt(parsedLog.args[4]).toString())
       isDeposited = await contract.deposited(hashOffChain)
       expect(isDeposited).toEqual(true)
+
+      const messageString = hashOffChain
+      const { signs, signers } = signMessage(messageString, members)
+      const groupsSign = new bls.Signature()
+      groupsSign.recover(signs, signers)
+
+
+      const signatureHex = groupsSign.serializeToHexStr()
+
+      const M = bls.hashToPoint(messageString)
+
+      const signature = bls.deserializeHexStrToG1(signatureHex)
+
+      let message_ser = bls.g1ToBN(M)
+
+      let sig_ser = bls.g1ToBN(signature)
+      let res = await contract.verifySignature(sig_ser, pubkey_ser, message_ser, messageString, parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]), BigInt(parsedLog.args[3]), BigInt(parsedLog.args[4]))
+      expect(res).toEqual(true)
+      //await expect(contract.verifySignature(sig_ser, member_pub_key_g2, message_ser)).rejects.toThrow(/Invalid Public Key/)
+
     }
     expect(await provider.getBalance(await contract.getAddress())).toEqual(amount)
   })
