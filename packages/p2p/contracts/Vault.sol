def87d345e4b056946fd45c71fd70455a3746e34
sachetsingh77 <sachet@leverj.io>
2024-06-18 11:30:17 +0530

    off chain hash of deposit args

--- packages/p2p/contracts/Vault.sol
+++ packages/p2p/contracts/Vault.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-
+import "hardhat/console.sol";
 contract Vault {
 
     address constant public ETH = address(0);
@@ -23,6 +23,7 @@ contract Vault {
         uint counter = depositCounter++;
         bytes32 hash = keccak256(abi.encodePacked(msg.sender, ETH, toChainId, msg.value, counter));
         deposited[hash] = true;
+        console.logBytes32(hash);
         emit Deposited(msg.sender, ETH, toChainId, msg.value, counter);
     }
     //This function will be called both externally and internally, hence public. 
@@ -30,6 +31,10 @@ contract Vault {
     function isDeposited(address depositor, address token, uint toChainId, uint amount, uint counter) public view returns (bool){
         bytes32 hash = keccak256(abi.encodePacked(depositor, token, toChainId, amount, counter));
         return deposited[hash];
+    }
+    //Utility function for unit testing
+    function hashOf(address depositor, address token, uint toChainId, uint amount, uint counter) public view returns (bytes32){
+        return keccak256(abi.encodePacked(depositor, token, toChainId, amount, counter));
     }
 
     function depositToken(uint amount) external {
--- packages/p2p/test/vault.spec.js
+++ packages/p2p/test/vault.spec.js
@@ -2,7 +2,7 @@ import {expect} from 'expect'
 import {createVault, provider} from './help/vault.js'
 import {Interface} from 'ethers'
 import vaultAbi from '../src/abi/Vault.json' assert {type: 'json'}
-
+import {buffersToHex, keccak256, toBuffer, uint} from '@leverj/gluon-plasma.common/src/utils/ethereum.js'
 describe('vault contract', function () {
 
   it.only('should be able to deposit ether', async function () {
@@ -20,6 +20,10 @@ describe('vault contract', function () {
       expect(isDeposited).toEqual(false)
       isDeposited = await contract.isDeposited(parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]), BigInt(parsedLog.args[4]), BigInt(parsedLog.args[3]))
       expect(isDeposited).toEqual(false)
+      //Compute hash off-chain and check deposit status
+      const hashOffChain = keccak256(parsedLog.args[0], parsedLog.args[1], BigInt(parsedLog.args[2]).toString(), BigInt(parsedLog.args[3]).toString(), BigInt(parsedLog.args[4]).toString())
+      isDeposited = await contract.deposited(hashOffChain)
+      expect(isDeposited).toEqual(true)
     }
     expect(await provider.getBalance(await contract.getAddress())).toEqual(amount)
   })
