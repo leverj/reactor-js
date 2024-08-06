24cf285dc62bb1af83703ab6b9737a0f2a540184
Benny Sadeh <benny.sadeh@gmail.com>
2024-08-06 16:31:34 -0700

    improve contracts, 11 Vault; streamlining

--- packages/chain/contracts/Vault.sol
+++ packages/chain/contracts/Vault.sol
@@ -2,11 +2,10 @@
 pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {Address, IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import {ERC20Proxy} from "./token/ERC20/ERC20Proxy.sol";
-import {ITokenProxy} from "./token/ITokenProxy.sol";
+//import {ITokenProxy} from "./token/ITokenProxy.sol";
 import {BlsVerifier} from "./BlsVerifier.sol";
 
 contract Vault {
@@ -32,13 +31,13 @@ contract Vault {
 
     uint public homeChain;
     uint[4] public publicKey;
-    mapping(address => uint) public pool;
+    BlsVerifier private verifier;
+    uint public sendCounter = 0;
+    mapping(address => uint) public balances;
     mapping(bytes32 => bool) public tokenSent;
     mapping(bytes32 => bool) public tokenArrived;
     mapping(uint => mapping(address => address)) public proxyMapping;
     mapping(address => bool) public isProxyMapping;
-    uint public sendCounter = 0;
-    BlsVerifier private verifier;
 
     constructor(uint chain_, uint[4] memory publicKey_) {
         homeChain = chain_;
@@ -57,7 +56,7 @@ contract Vault {
     }
 
     function sendNative(uint toChain) external payable {
-        pool[NATIVE] += msg.value;
+        balances[NATIVE] += msg.value;
         Chain storage chain = chains[homeChain];
         send(homeChain, NATIVE, chain.name, chain.symbol, chain.decimals, msg.value, msg.sender, homeChain, toChain);
     }
@@ -71,46 +70,34 @@ contract Vault {
         else {
             ERC20 erc20 = ERC20(token);
             safeTransfer(token, msg.sender, address(this), amount);
-            pool[token] += amount;
+            balances[token] += amount;
             send(homeChain, token, erc20.name(), erc20.symbol(), erc20.decimals(), amount, msg.sender, homeChain, toChain);
         }
     }
 
     function send(uint originatingChain, address token, string memory name, string memory symbol, uint decimals, uint amount, address owner, uint fromChain, uint toChain) private {
         sendCounter++;
-        bytes32 hash = payloadHash(originatingChain, token, decimals, amount, owner, fromChain, toChain, sendCounter);
+        bytes32 hash = keccak256(abi.encode(originatingChain, token, decimals, amount, owner, fromChain, toChain, sendCounter));
         tokenSent[hash] = true;
         emit TokenSent(originatingChain, token, name, symbol, decimals, amount, owner, fromChain, toChain, sendCounter);
     }
 
-    function tokenArrival(uint[2] calldata signature, uint[4] calldata signerKey, bytes calldata payload, string calldata name, string calldata symbol) public {
+    function tokenArrival(uint[2] calldata signature, uint[4] calldata signerKey, bytes calldata payload, string calldata name, string calldata symbol) external {
         bytes32 tokenSendHash = keccak256(payload);
         require(tokenArrived[tokenSendHash] == false, 'Token Arrival already processed');
-        validatePayloadAndSignature(signature, signerKey, tokenSendHash);
-        mintOrDisburse(payload, name, symbol);
+        for (uint i = 0; i < 4; i++) require(publicKey[i] == signerKey[i], 'Invalid Public Key'); // validate signerKey
+        verifier.validate(signature, signerKey, tokenSendHash);
+        //fixme: what's the point of the extra 3 fields in payload? (if they are never used)
+        (uint chain, address token, uint8 decimals, uint amount, address owner, , ,) = abi.decode(payload, (uint, address, uint8, uint, address, uint, uint, uint));
+        mintOrDisburse(chain, token, decimals, amount, owner, name, symbol);
         tokenArrived[tokenSendHash] = true;
     }
 
-    function payloadHash(uint originatingChain, address token, uint decimals, uint amount, address owner, uint fromChain, uint toChain, uint counter) public pure returns (bytes32) {
-        return keccak256(abi.encode(originatingChain, token, decimals, amount, owner, fromChain, toChain, counter));
-    }
-
-    function validatePayloadAndSignature(uint[2] memory signature, uint[4] memory signerKey, bytes32 hash) private view {
-        validateSignerKey(signerKey);
-        verifier.validate(signature, signerKey, hash);
-    }
-
-    function validateSignerKey(uint[4] memory signerKey) private view {
-        for (uint i = 0; i < 4; i++) require(publicKey[i] == signerKey[i], 'Invalid Public Key');
-    }
-
-    function mintOrDisburse(bytes calldata payload, string calldata name, string calldata symbol) private {
-        //fixme: what's the point of the extra 3 fields in payload?
-        (uint chain, address token, uint8 decimals, uint amount, address owner, , ,) = abi.decode(payload, (uint, address, uint8, uint, address, uint, uint, uint));
+    function mintOrDisburse(uint chain, address token, uint8 decimals, uint amount, address owner, string calldata name, string calldata symbol) private {
         if (homeChain == chain) {  // if token is coming back home (to originating chain) ...
             // ... disburse amount back to the owner
-            pool[token] -= amount; // fixme: require enough amount?
-            safeTransfer(token, address(this), owner, amount);
+            balances[token] -= amount; // fixme: require enough amount?
+            safeTransfer(token, address(this), owner, amount); //fixme: is this called for when token is NATIVE ?
         } else {
             // ... it's a foreign country, mint proxy for the owner
             if (proxyMapping[chain][token] == address(0)) { // if proxy does not exist
@@ -122,7 +109,7 @@ contract Vault {
     }
 
     function safeTransfer(address token, address from, address to, uint amount) private {
-        if (token == address(0)) payable(to).sendValue(amount); //fixme: Consider using {ReentrancyGuard}
+        if (token == NATIVE) payable(to).sendValue(amount); //fixme: Consider using {ReentrancyGuard}
         else {
             IERC20 erc20 = ERC20(token);
             uint balance = erc20.balanceOf(to);
