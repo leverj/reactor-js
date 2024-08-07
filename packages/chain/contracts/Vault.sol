5513708f26c98f6ce606c632582dd5887fadb57b
Benny Sadeh <benny.sadeh@gmail.com>
2024-08-07 10:42:31 -0700

    improve contracts, 12 Vault; reentrancy

--- packages/chain/contracts/Vault.sol
+++ packages/chain/contracts/Vault.sol
@@ -4,11 +4,12 @@ pragma solidity ^0.8.20;
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Address, IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {IERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {ERC20Proxy} from "./token/ERC20/ERC20Proxy.sol";
 //import {ITokenProxy} from "./token/ITokenProxy.sol";
 import {BlsVerifier} from "./BlsVerifier.sol";
 
-contract Vault {
+contract Vault is ReentrancyGuard{
     using Address for address payable;
     using SafeERC20 for IERC20;
 
@@ -43,12 +44,12 @@ contract Vault {
         homeChain = chain_;
         publicKey = publicKey_;
         verifier = new BlsVerifier();
-        chains[homeChain] = Chain('ETHER', 'ETH', 18); //fixme: generalize setup, and make it by owner
+        chains[homeChain] = Chain('ETHER', 'ETH', 18);
     }
 
-    function addChain(uint chainId, string calldata name, string calldata symbol, uint decimals) public {
-        chains[chainId] = Chain(name, symbol, decimals);
-    }
+//    function addChain(uint chainId, string calldata name, string calldata symbol, uint decimals) public { //fixme: generalize setup, and make it by owner
+//        chains[chainId] = Chain(name, symbol, decimals);
+//    }
 
     function isProxy(address token) public view returns (bool) {
         return isProxyMapping[token];
@@ -83,33 +84,32 @@ contract Vault {
     }
 
     function tokenArrival(uint[2] calldata signature, uint[4] calldata signerKey, bytes calldata payload, string calldata name, string calldata symbol) external {
-        bytes32 tokenSendHash = keccak256(payload);
-        require(tokenArrived[tokenSendHash] == false, 'Token Arrival already processed');
+        bytes32 hash = keccak256(payload);
+        require(tokenArrived[hash] == false, 'Token Arrival already processed');
         for (uint i = 0; i < 4; i++) require(publicKey[i] == signerKey[i], 'Invalid Public Key'); // validate signerKey
-        verifier.validate(signature, signerKey, tokenSendHash);
-        //fixme: what's the point of the extra 3 fields in payload? (if they are never used)
-        (uint chain, address token, uint8 decimals, uint amount, address owner, , ,) = abi.decode(payload, (uint, address, uint8, uint, address, uint, uint, uint));
-        mintOrDisburse(chain, token, decimals, amount, owner, name, symbol);
-        tokenArrived[tokenSendHash] = true;
+        verifier.validate(signature, signerKey, hash);
+        (uint fromChain, address token, uint8 decimals, uint amount, address owner, , ,) = abi.decode(payload, (uint, address, uint8, uint, address, uint, uint, uint));
+        mintOrDisburse(fromChain, token, decimals, amount, owner, name, symbol);
+        tokenArrived[hash] = true;
     }
 
-    function mintOrDisburse(uint chain, address token, uint8 decimals, uint amount, address owner, string calldata name, string calldata symbol) private {
-        if (homeChain == chain) {  // if token is coming back home (to originating chain) ...
+    function mintOrDisburse(uint fromChain, address token, uint8 decimals, uint amount, address owner, string calldata name, string calldata symbol) private {
+        if (homeChain == fromChain) {  // if token is coming back home (to originating chain) ...
             // ... disburse amount back to the owner
             balances[token] -= amount; // fixme: require enough amount?
             safeTransfer(token, address(this), owner, amount); //fixme: is this called for when token is NATIVE ?
         } else {
             // ... it's a foreign country, mint proxy for the owner
-            if (proxyMapping[chain][token] == address(0)) { // if proxy does not exist
-                proxyMapping[chain][token] = address(new ERC20Proxy(name, symbol, decimals, token, chain));
-                isProxyMapping[proxyMapping[chain][token]] = true;
+            if (proxyMapping[fromChain][token] == address(0)) { // if proxy does not exist
+                proxyMapping[fromChain][token] = address(new ERC20Proxy(name, symbol, decimals, token, fromChain));
+                isProxyMapping[proxyMapping[fromChain][token]] = true;
             }
-            ERC20Proxy(proxyMapping[chain][token]).mint(owner, amount);
+            ERC20Proxy(proxyMapping[fromChain][token]).mint(owner, amount);
         }
     }
 
-    function safeTransfer(address token, address from, address to, uint amount) private {
-        if (token == NATIVE) payable(to).sendValue(amount); //fixme: Consider using {ReentrancyGuard}
+    function safeTransfer(address token, address from, address to, uint amount) private nonReentrant {
+        if (token == NATIVE) payable(to).sendValue(amount);
         else {
             IERC20 erc20 = ERC20(token);
             uint balance = erc20.balanceOf(to);
