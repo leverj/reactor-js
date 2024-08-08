fcd942ae1954d30215fb3f079a0f43462ee84e2f
Benny Sadeh <benny.sadeh@gmail.com>
2024-08-08 16:45:50 -0700

    cleanup BridgeNode.spec.js

--- packages/p2p/test/BridgeNode.spec.js
+++ packages/p2p/test/BridgeNode.spec.js
@@ -6,15 +6,14 @@ import {peerIdJsons} from './help/index.js'
 describe('BridgeNode', () => {
   const nodes = []
 
-  const stopBridgeNodes = async () => {
+  afterEach(async () => {
     for (const node of nodes) await node.stop()
     nodes.length = 0
-  }
+  })
 
   const createBridgeNodes = async (count) => {
     const bootstraps = []
     for (let i = 0; i < count; i++) {
-      // fixme: get peerid from config eventually some file
       const node = new BridgeNode({
         port: 9000 + i,
         isLeader: i === 0,
@@ -25,38 +24,36 @@ describe('BridgeNode', () => {
       nodes.push(node)
       if (i === 0) bootstraps.push(node.multiaddrs[0])
     }
-    return nodes
   }
-
-  afterEach(async () => await stopBridgeNodes())
 
   it('should race connect multiple nodes with each other', async () => {
     //Starts breaking beyond 6 nodes. works fine till 6
-    const nodes = await createBridgeNodes(6)
+    await createBridgeNodes(6)
     for (const node of nodes) {
       for (const peer of nodes) {
         if (node.multiaddrs[0] === peer.multiaddrs[0]) continue
         await node.connect(peer.peerId)
       }
     }
+    //fixme: where are the asserts?
   })
 
   it('it should be able to connect with other nodes', async () => {
-    const [leader, node1, node2, node3, node4, node5, node6] = await createBridgeNodes(7)
-    const nodes = [leader, node1, node2, node3, node4, node5, node6]
-    // whitelisted nodes
-    await leader.publishWhitelist()
-    await setTimeout(100)
-    for (const node of nodes) expect(node.whitelist.get().length).toEqual(7)
+    const howMany = 7
+    await createBridgeNodes(howMany)
+    const leader = nodes[0]
+    await leader.publishWhitelist() // whitelisted nodes
+    for (const node of nodes) expect(node.whitelist.get().length).toEqual(howMany)
+
     await leader.startDKG(4)
-    await setTimeout(2000)
+    const leaderSecretKey = leader.tssNode.secretKeyShare.serializeToHexStr()
+    const leaderGroupKey = leader.tssNode.groupPublicKey.serializeToHexStr()
+    await setTimeout(100)
     for (const node of nodes) {
       node.tssNode.print()
-      expect(leader.tssNode.groupPublicKey.serializeToHexStr()).toEqual(node.tssNode.groupPublicKey.serializeToHexStr())
-      const leaderSecret = leader.tssNode.secretKeyShare.serializeToHexStr()
       if (leader.peerId === node.peerId) continue
-      const nodeSecret = node.tssNode.secretKeyShare.serializeToHexStr()
-      expect(leaderSecret).not.toBe(nodeSecret)
+      expect(leaderGroupKey).toEqual(node.tssNode.groupPublicKey.serializeToHexStr())
+      expect(leaderSecretKey).not.toBe(node.tssNode.secretKeyShare.serializeToHexStr())
     }
   })
 })
