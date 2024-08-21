b3c229c1123df53cdf7065e05e3a33153dfa72b1
Nirmal Gupta <nirmalk.gupta@gmail.com>
2024-08-21 15:05:05 -0500

    cleanup

--- packages/p2p/scripts/orchestrate.sh
+++ packages/p2p/scripts/orchestrate.sh
@@ -34,7 +34,6 @@ function deployDocker() {
       -e COMPONENTS=[] \
       -e FAIL=true \
       -e TRY_COUNT=50 \
-      -e BRIDGE_IS_PUBLIC=$BRIDGE_IS_PUBLIC \
       -p $PORT:$PORT \
       -p $BRIDGE_PORT:$BRIDGE_PORT \
       -v $DATA_DIR/$PORT:/dist/data \
@@ -68,12 +67,11 @@ function local_install() {
     local COUNT=$1
     local BRIDGE_THRESHOLD=$(($COUNT/2 + 1))
     local EXTERNAL_IP=$(ifconfig en0 inet | grep  inet |awk '{print $2}')
-    local BRIDGE_BOOTSTRAP_NODE=http://$EXTERNAL_IP:9000
     rm -rf ../.node.ignore
     local total=$(($COUNT + 9000))
     docker ps -aq -f NAME=p2p-node | xargs docker stop | xargs docker rm
 #    echo \
-    EXTERNAL_IP=$EXTERNAL_IP BRIDGE_IS_PUBLIC=true BRIDGE_THRESHOLD=$BRIDGE_THRESHOLD BRIDGE_BOOTSTRAP_NODE=$BRIDGE_BOOTSTRAP_NODE \
+    EXTERNAL_IP=$EXTERNAL_IP BRIDGE_THRESHOLD=$BRIDGE_THRESHOLD \
       DATA_DIR=${PWD}/../.node.ignore deployDocker 9000 $(($COUNT + 9000 - 1))
 
     sleep 10
@@ -105,7 +103,6 @@ function remote_install() {
     COUNT=$(($EACH_COUNT*$IP_COUNT))
     local BRIDGE_THRESHOLD=$(($COUNT/2 + 1))
     local FIRST_IP=$(echo $REMOTE_IPS | cut -d, -f1)
-    local BRIDGE_BOOTSTRAP_NODE=$(remote_leader)
     local START=9000
     for IP in $(echo $REMOTE_IPS | tr ',' '\n'); do
       local EXTERNAL_IP=$IP
@@ -118,7 +115,7 @@ function remote_install() {
       "
       [[ $? -ne 0 ]] && echo "no docker containers on $EXTERNAL_IP"
 #      echo \
-      REMOTE=true EXTERNAL_IP=$EXTERNAL_IP BRIDGE_IS_PUBLIC=true BRIDGE_THRESHOLD=$BRIDGE_THRESHOLD BRIDGE_BOOTSTRAP_NODE=$BRIDGE_BOOTSTRAP_NODE \
+      REMOTE=true EXTERNAL_IP=$EXTERNAL_IP BRIDGE_THRESHOLD=$BRIDGE_THRESHOLD \
         DATA_DIR=/var/lib/reactor/data  deployDocker $START $END
       START=$(($END + 1))
     done
