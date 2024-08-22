366451c14568d39a233639007da8b51584adb274
Benny Sadeh <benny.sadeh@gmail.com>
2024-08-22 15:44:43 -0700

    e2e.spec, 4

--- packages/p2p/src/rest/manager.js
+++ packages/p2p/src/rest/manager.js
@@ -1,13 +1,18 @@
 import config from 'config'
-import JSONStore from 'json-store'
 import {existsSync, readFileSync, writeFileSync, mkdirSync} from 'node:fs'
 import {BridgeNode} from '../BridgeNode.js'
 import {events, INFO_CHANGED} from '../utils/index.js'
 
-const {bridgeNode: {confDir, port, bootstrapNodes}} = config
-mkdirSync(confDir, {recursive: true})
-const infoFile = `${confDir}/info.json`
-const store = JSONStore(infoFile)
+const {bridgeNode: {port, bootstrapNodes}} = config
+
+const e2ePath = `${process.cwd()}/../../data/.e2e`
+const dirPath = (port) => `${e2ePath}/${port}`
+const filePath = (port) => `${dirPath(port)}/info.json`
+const getInfo = (port) => existsSync(filePath(port)) ? JSON.parse(readFileSync(filePath(port)).toString()) : undefined
+const setInfo = (port, info) => {
+  if (!existsSync(dirPath(port))) mkdirSync(dirPath(port), {recursive: true})
+  writeFileSync(filePath(port), JSON.stringify(info))
+}
 
 class Info {
   constructor(node, data) {
@@ -24,14 +29,12 @@ class Info {
     this.timer = setTimeout(() => {
       if (this.data === this.node.info()) return
       this.data = this.node.info()
-      writeFileSync(infoFile, JSON.stringify(this.data))
-      // store.set(port, this.data)
+      setInfo(config.port, this.data)
     }, 10)
   }
 }
 
-const data = existsSync(infoFile) ? JSON.parse(readFileSync(infoFile, 'utf8')) : undefined
-// const data = store.get(port)
+const data = getInfo(config.port)
 const manager = await BridgeNode.from(port, bootstrapNodes, data)
 new Info(manager, data)
 await manager.start()
--- packages/p2p/test/e2e.spec.js
+++ packages/p2p/test/e2e.spec.js
@@ -1,37 +1,90 @@
+import {logger} from '@leverj/common/utils'
+import axios from 'axios'
+import {fork} from 'child_process'
+import config from 'config'
 import {expect} from 'expect'
-import JSONStore from 'json-store'
-import {mkdirSync, rmdirSync} from 'node:fs'
+import {existsSync, mkdirSync, readFileSync, rmdirSync, writeFileSync} from 'node:fs'
 import {setTimeout} from 'node:timers/promises'
+import {tryAgainIfError, waitToSync} from '../src/utils/index.js'
 import {getNodeInfos} from './help/fixtures.js'
-import {
-  createApiNodes,
-  createApiNodesFrom,
-  createNodeInfos,
-  e2ePath,
-  GET,
-  getInfo,
-  LEADER_PORT,
-  POST,
-  publishWhitelist,
-  stop,
-  waitForWhitelistSync,
-} from './help/e2e.js'
+
+const {bridgeNode, externalIp} = config
+const e2ePath = `${process.cwd()}/../../data/.e2e`
+const leaderPort = config.port
+
+const dirPath = (port) => `${e2ePath}/${port}`
+const filePath = (port) => `${dirPath(port)}/info.json`
+const getInfo = (port) => JSON.parse(readFileSync(filePath(port)).toString())
+const setInfo = (port, info) => {
+  if (!existsSync(dirPath(port))) mkdirSync(dirPath(port), {recursive: true})
+  writeFileSync(filePath(port), JSON.stringify(info))
+}
+
+const GET = (port, endpoint) => axios.get(`http://127.0.0.1:${port}/api/${endpoint}`).then(_ => _.data)
+const POST = (port, endpoint, payload) => axios.post(`http://127.0.0.1:${port}/api/${endpoint}`, payload || {})
+
+async function waitForWhitelistSync(ports, howMany = ports.length) {
+  await waitToSync(ports.map(_ => async () => GET(_, 'whitelist').then(_ => _.length === howMany)))
+  logger.log('whitelisted synced...')
+}
+
+const publishWhitelist = async (ports, total, available) =>
+  tryAgainIfError(_ => POST(leaderPort, 'whitelist/publish')).then(_ => waitForWhitelistSync(ports, total, available))
+
+const createNodeInfos = (howMany) => getNodeInfos(howMany).forEach((info, i) => setInfo(leaderPort + i, info))
 
 describe.only('e2e', () => {
-  let store
+  const processes = {}
 
   beforeEach(async () => {
     mkdirSync(e2ePath, {recursive: true})
-    store = JSONStore(`${e2ePath}/info.json`)
   })
   afterEach(async () => {
     await stop()
     rmdirSync(e2ePath, {recursive: true, force: true})
   })
 
+  async function stop(ports = Array.from(Object.keys(processes))) {
+    for (let each of ports) {
+      processes[each].kill()
+      delete processes[each]
+    }
+    await setTimeout(100)
+  }
+
+  async function createApiNodesFrom(ports, howMany = ports.length - 1) {
+    const createApiNode = async (port) => {
+      const index = port - leaderPort
+      const bootstrapNodes = port === leaderPort ?
+        [] :
+        await tryAgainIfError(_ => getInfo(leaderPort).p2p.id).then(leader => [`/ip4/${externalIp}/tcp/${bridgeNode.port}/p2p/${leader}`])
+      const env = Object.assign({}, process.env, {
+        PORT: port,
+        BRIDGE_PORT: bridgeNode.port + index,
+        BRIDGE_BOOTSTRAP_NODES: JSON.stringify(bootstrapNodes),
+      })
+      return fork('app.js', [], {cwd: process.cwd(), env})
+    }
+
+    for (let each of ports) {
+      processes[each] = await createApiNode(each)
+      await setTimeout(10)
+    }
+    await waitToSync(ports.map(_ => async () => GET(_, 'peer').then(_ => _.length === howMany)))
+    logger.log('bootstrap synced...')
+    return ports
+  }
+
+  async function createApiNodes(howMany, whitelist = true) {
+    const ports = new Array(howMany).fill(0).map((_, i) => leaderPort + i)
+    await createApiNodesFrom(ports)
+    if (whitelist) await publishWhitelist(ports)
+    return ports
+  }
+
   it('should create new nodes, connect and init DKG', async () => {
     const ports = await createApiNodes(2)
-    await POST(LEADER_PORT, 'dkg/start')
+    await POST(leaderPort, 'dkg/start')
     await setTimeout(100)
     const publicKeys = ports.map(_ => getInfo(_).tssNode.groupPublicKey)
     for (let each of publicKeys) {
@@ -44,21 +97,21 @@ describe.only('e2e', () => {
     await createNodeInfos(3)
     const infos = getNodeInfos(3)
     const ports = await createApiNodes(3)
-    for (let [i, port] of Object.entries(ports)) expect(getInfo(port)).toEqual(infos[i])
+    for (let [i, port] of ports.entries()) expect(getInfo(port)).toEqual(infos[i])
   })
 
   it('aggregate signatures over pubsub topic', async () => {
     await createNodeInfos(4)
     await createApiNodes(4)
     const txnHash = 'hash123456'
-    await POST(LEADER_PORT, 'tss/aggregateSign', {msg: txnHash})
+    await POST(leaderPort, 'tss/aggregateSign', {msg: txnHash})
     await setTimeout(100)
-    expect(await GET(LEADER_PORT, `tss/aggregateSign?txnHash=${txnHash}`).then(_ => _.verified)).toEqual(true)
+    expect(await GET(leaderPort, `tss/aggregateSign?txnHash=${txnHash}`).then(_ => _.verified)).toEqual(true)
   })
 
   it('whitelist', async () => {
     const ports = await createApiNodes(4, false)
-    await GET(LEADER_PORT + 1, 'peer/bootstrapped')
+    await GET(leaderPort + 1, 'peer/bootstrapped')
     await stop(ports.slice(2))
     await publishWhitelist(ports.slice(0, 2), 4)
     expect(getInfo(ports[0]).whitelist).toHaveLength(4)
