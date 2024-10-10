7209fb992a44562d6b503eaaf7e184c5c30dfead
Benny Sadeh <benny.sadeh@gmail.com>
2024-10-09 17:52:01 -0700

    configuration refinements, 2

--- packages/p2p/config/defaults.js
+++ packages/p2p/config/defaults.js
@@ -1,11 +1,7 @@
-import info from '../package.json' assert {type: 'json'}
-import {last} from 'lodash-es'
-
-const dataDir = `${import.meta.dirname}/../../../data`
-export const packageName = last(info.name.split('/'))
+export const dataDir = `${import.meta.dirname}/../../../data`
 
 export const defaults = {
   bridge: {
-    nodesDir: function () { return `${dataDir}/${packageName}/${this.env}` },
+    nodesDir: function () { return `${dataDir}/p2p/${this.env}` },
   }
 }
